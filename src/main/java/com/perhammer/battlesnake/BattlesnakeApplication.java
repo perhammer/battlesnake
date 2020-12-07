f26011733bc393b49f8629be06c44e768e3a73fc
perhammer <github@perhammer.com>
2020-12-07 21:06:35 +0000

    From Spring Initializr

---
+++ src/main/java/com/perhammer/battlesnake/BattlesnakeApplication.java
@@ -0,0 +1,13 @@
+package com.perhammer.battlesnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlesnakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlesnakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perhammer/battlesnake/BattlesnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perhammer.battlesnake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesnakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
